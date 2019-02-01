e5ec6314da778ee44944a36d4218b73cf3cc27d5
Yuheng Zhang <zhangyuheng91@gmail.com>
2019-02-01 18:30:24 +0800

    feat: init

---
+++ bin/cli.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+const path = require('path');
+const sao = require('sao');
+const cli = require('cac')();
+
+require('./checkUpdate')();
+
+cli
+  .command('<target-folder>', 'Generate a new project to target folder')
+  .option(
+    '--npm-client <client>',
+    'Choose an npm client for installing packages (\'tnpm\' | \'yarn\')'
+  )
+  .option(
+    '--registry <registry>',
+    'Use a custom registry for package manager'
+  )
+  .action(async (targetFolder, { npmClient, registry }) => {
+    const app = sao({
+      generator: path.join(__dirname, '..'),
+      outDir: targetFolder,
+      npmClient,
+      registry,
+    });
+
+    await app.run().catch(sao.handleError);
+  });
+
+cli.on('command:!', () => {
+  if (!cli.args.length) cli.outputHelp();
+});
+
+cli.help();
+cli.version(require('../package').version);
+
+cli.parse();
---
+++ template/README.md
@@ -0,0 +1,7 @@
+# <%= name %>
+
+> <%= description %>
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
